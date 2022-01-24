e92c69f4f7facb5b40bb3c0b284a5e4b2e438af3
theo-grivel <togriv@gmail.com>
2022-01-24 18:41:15 +0100

    wasn't working good

--- plug.sh
+++ plug.sh
@@ -1,14 +1,18 @@
 ###	Install Neovim
 
-sudo apt install neovim
+sudo apt update
+yes | sudo apt upgrade
+yes | sudo apt install neovim
 
 
 ###	Install plugin for neovim
 
-DIR=$HOME"/.local/share/nvim/site/autoload/plug.vim"
+DIR=$HOME"/.local/share/nvim/site/autoload/"
+FILE=$DIR"plug.vim"
 URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
-wget $URL -O $DIR
+mkdir -p $DIR
+wget $URL -O $FILE
 
 
 ###	Move configuration files in the good directories
@@ -31,4 +35,4 @@ mv ./nvim $DIR/
 # https://github.com/nodesource/distributions/blob/master/README.md
 
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
+yes | sudo apt-get install -y nodejs
